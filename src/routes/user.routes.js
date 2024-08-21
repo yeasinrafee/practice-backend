d3c72e9e5a0f329dde9933ed0d8cfb3c45bf812a
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-08-21 23:51:59 +0600

    bug fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,6 +10,7 @@ import {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "./../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
