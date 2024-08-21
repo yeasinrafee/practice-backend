ccf5e290aff9266c95e69b3e0ba4c4be9ab43589
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-08-21 18:03:25 +0600

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = model("Subscription", subscriptionSchema);
